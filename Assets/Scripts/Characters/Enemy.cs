f86b3cfad9da74bdae6b599bc482c62df8f30e5a
Matvey <tod13yung@ya.ru>
2020-12-21 16:14:26 +0400

    Finished prefabs

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class Enemy : MonoBehaviour, ICharacter{
 
     public Animator anim;
+    public float attackInterval = 2f;
+    public GameObject missile;
+
+    void Start(){
+        InvokeRepeating("Attack", attackInterval, attackInterval);
+    }
+
+    void Attack(){
+        if(missile){
+            Instantiate(missile, transform.position - new Vector3(1f, 0f), transform.rotation);
+        }
+    }
 
     public void Move(Vector3 movement){
         transform.Translate(movement);
